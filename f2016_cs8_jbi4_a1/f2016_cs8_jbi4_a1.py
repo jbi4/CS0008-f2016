014662cc79565f9957fc9cd0235dc1b25f87c2ee
joeisaacson <jbi4@pitt.edu>
2016-09-28 13:10:00 -0400

    Start of assignment 1, added heading and the initial inputs for the unit of measure, distance driven, and gas usage Update added a check to make sure inputs for distance driven and gas usage were positive, along with adding the fuel consumption in both units of measure and the rating using the table in the assignent

--- f2016_cs8_jbi4_a1/f2016_cs8_jbi4_a1.py
+++ f2016_cs8_jbi4_a1/f2016_cs8_jbi4_a1.py
@@ -13,18 +13,36 @@
 
 #Start by asking what unit of measure the user would like to enter the data as
 uom = input("What unit of measure would you like to use, USC or Metric?")
-#Have the user input the distance driven and gas used in metrics and convert to USC
+#Have the user input the distance driven and gas used in metrics, check to make sure inputs are positive, and convert to USC
 if uom == "Metric" or uom == "metric":
     ddm = float(input("Distance driven in Km?"))
     gum = float(input("Gas used in Liters?"))
+    if ddm < 0 or gum < 0:
+        print("Error: Invalid inputs.")
     ddu = ddm * .621371
     guu = gum * .264172
-#Have the user input the distance driven and gas used in USC and convert to metric
+#Have the user input the distance driven and gas used in USC, check to make sure inputs are positive, and convert to metric
 elif uom == "USC" or uom == "usc":
     ddu = float(input("Distance driven in miles?"))
     guu = float(input("Gas used in gallons?"))
+    if ddu < 0 or guu < 0:
+        print("Error: Invalid inputs.")
     ddm = ddu * 1.60934
     gum = guu * 3.78541
 #print error if an invalic unit of measure was given
 else:
     print("Error: Invalid unit of measure.")
+#calculate the fuel consumption in metric and usc
+fcm = 100 * gum / ddm
+fcu = ddu / guu
+#use the fuel consumption in metrics to calculate the gas consumption rating
+if fcm > 20:
+    gcr = "Extremely poor"
+elif fcm > 15 and fcm <= 20:
+    gcr = "Poor"
+elif fcm > 10 and fcm <= 15:
+    gcr = "Average"
+elif fcm > 8 and fcm <= 10:
+    gcr = "Good"
+else:
+    gcr = "Excellent"
