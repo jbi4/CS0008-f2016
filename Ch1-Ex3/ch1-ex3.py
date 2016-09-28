f33f67f922714ba3ccf38433ecda0e30e01194ee
joeisaacson <jbi4@pitt.edu>
2016-09-28 12:55:33 -0400

    Start of assignment 1, added heading and the initial inputs for the unit of measure, distance driven, and gas usage

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,15 @@
+#
+# Template for code submission
+# name : Joseph Isaacson
+# email : jbi4@pitt.edu
+# date :
+# class : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# description of this file goes here
+# Example: Starting with Python, Chapter 1, Exercise 3
+#
+# Notes:
+# any notes to the instructor and/or TA goes here
+# ...and now let's program with Python
