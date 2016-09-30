e736ed41f5478ddb252a430fb06ae634a69c8565
joeisaacson <jbi4@pitt.edu>
2016-09-30 13:17:05 -0400

    Start of assignment 1, added heading and the initial inputs for the unit of measure, distance driven, and gas usage Update added a check to make sure inputs for distance driven and gas usage were positive, along with adding the fuel consumption in both units of measure and the rating using the table in the assignment formatted the printing of the output fixed the error message if the user inputs 0 or a negative number

--- f2016_cs8_jbi4_a1/f2016_cs8_jbi4_a1.py
+++ f2016_cs8_jbi4_a1/f2016_cs8_jbi4_a1.py
@@ -8,18 +8,20 @@
 # Description: First assignment to help a client be able to better understand gas mileage
 # Example: Assignment 1 Exercise 16 on the test
 #
-# Notes:
+# Notes: I know there is a better was than using all of those tabs in my print function, but I do not remember it.
 
 #Set all future variables to 0
 uom = 0
 ddm = 0
 gum = 0
 ddu = 0
+guu = 0
 fcm = 0
 fcu = 0
 #Start by asking what unit of measure the user would like to enter the data as
 uom = input("What unit of measure would you like to use, USC or Metric?")
-#Have the user input the distance driven and gas used in metrics, check to make sure inputs are positive, and convert to USC
+#Have the user input the distance driven and gas used in metrics, check to make sure inputs are positive, and
+#convert to USC
 if uom == "Metric" or uom == "metric":
     ddm = float(input("Distance driven in Km?"))
     gum = float(input("Gas used in Liters?"))
@@ -27,7 +29,8 @@ if uom == "Metric" or uom == "metric":
         print("Error: Invalid inputs.")
     ddu = ddm * .621371
     guu = gum * .264172
-#Have the user input the distance driven and gas used in USC, check to make sure inputs are positive, and convert to metric
+#Have the user input the distance driven and gas used in USC, check to make sure inputs are positive, and
+#convert to metric
 elif uom == "USC" or uom == "usc":
     ddu = float(input("Distance driven in miles?"))
     guu = float(input("Gas used in gallons?"))
@@ -38,10 +41,22 @@ elif uom == "USC" or uom == "usc":
 #print error if an invalic unit of measure was given
 else:
     print("Error: Invalid unit of measure.")
-#calculate the fuel consumption in metric and usc
-fcm = 100 * gum / ddm
-fcu = ddu / guu
-#use the fuel consumption in metrics to calculate the gas consumption rating
+#check if the user put in a negative number or zero
+if ddm > 0 and gum > 0 and ddu > 0 and guu > 0:
+#if the inputs are valid, calculate the fuel consumption in metric and usc
+    fcm = 100 * gum / ddm
+    fcu = ddu / guu
+#for invalid inputs set everything to zero to print an error message later under the gas consumption rating
+else:
+    uom = 0
+    ddm = 0
+    gum = 0
+    ddu = 0
+    guu = 0
+    fcm = 0
+    fcu = 0
+#use the fuel consumption in metrics to calculate the gas consumption rating, the error message from above is printed
+#under the gas consumption rationg
 if fcm > 20:
     gcr = "Extremely poor"
 elif fcm > 15 and fcm <= 20:
